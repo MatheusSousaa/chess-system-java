20c580b60849059717dd5799fa076197ab8d6bc2
Matheus Sousa <theuzin131@gmail.com>
2026-08-19 09:15:27 -0300

    Fist class: Position

---
+++ src/boardGame/Position.java
@@ -0,0 +1,37 @@
+package boardGame;
+
+public class Position {
+
+    private int row;
+    private int column;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.column = col;
+
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{ " +
+                "row = " + row +
+                " , column = " + column +
+                " }";
+    }
+}
