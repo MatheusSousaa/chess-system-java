6c2dabe02c8ea31974b7ef2e85807a7b34ca9244
Matheus Sousa <theuzin131@gmail.com>
2026-08-20 11:12:42 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,12 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+
     public static ChessPosition readChessPosition(Scanner tec) {
         try {
             String s = tec.nextLine();
